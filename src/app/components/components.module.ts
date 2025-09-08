9f1d3e5792b4615347fdaae762ed61a6066da76f
j4vierlb <jlarabustos999@gmail.com>
2025-09-07 21:03:15 -0300

    Arreglos finales

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,43 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { LoginLogoComponent } from './login-logo/login-logo.component';
 import { LoginHeaderComponent } from './login-header/login-header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { LoginFooterComponent } from './login-footer/login-footer.component';
-import { ResetPasswordFormComponent } from './reset-password-form/reset-password-form.component';
-import { ResetPasswordSuccessComponent } from './reset-password-success/reset-password-success.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { HomeMetricsComponent } from './home-metrics/home-metrics.component';
 import { HomeQuickActionsComponent } from './home-quick-actions/home-quick-actions.component';
 import { HomeRecentActivitiesComponent } from './home-recent-activities/home-recent-activities.component';
+import { ResetPasswordFormComponent } from './reset-password-form/reset-password-form.component';
+import { ResetPasswordSuccessComponent } from './reset-password-success/reset-password-success.component';
 
 @NgModule({
-	declarations: [
-		LoginLogoComponent,
-		LoginHeaderComponent,
-		LoginFormComponent,
-		LoginFooterComponent,
-		ResetPasswordFormComponent,
-		ResetPasswordSuccessComponent,
-		HomeHeaderComponent,
-		HomeMetricsComponent,
-		HomeQuickActionsComponent,
-		HomeRecentActivitiesComponent
-	],
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, RouterModule],
-	exports: [
-		LoginLogoComponent,
-		LoginHeaderComponent,
-		LoginFormComponent,
-		LoginFooterComponent,
-		ResetPasswordFormComponent,
-		ResetPasswordSuccessComponent,
-		HomeHeaderComponent,
-		HomeMetricsComponent,
-		HomeQuickActionsComponent,
-		HomeRecentActivitiesComponent
-	]
+  declarations: [
+    LoginLogoComponent,
+    LoginHeaderComponent,
+    LoginFormComponent,
+    LoginFooterComponent,
+    HomeHeaderComponent,
+    HomeMetricsComponent,
+    HomeQuickActionsComponent,
+  HomeRecentActivitiesComponent,
+  ResetPasswordFormComponent,
+  ResetPasswordSuccessComponent
+  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule],
+  exports: [
+    LoginLogoComponent,
+    LoginHeaderComponent,
+    LoginFormComponent,
+    LoginFooterComponent,
+    HomeHeaderComponent,
+    HomeMetricsComponent,
+    HomeQuickActionsComponent,
+  HomeRecentActivitiesComponent,
+  ResetPasswordFormComponent,
+  ResetPasswordSuccessComponent
+  ]
 })
 export class ComponentsModule {}
