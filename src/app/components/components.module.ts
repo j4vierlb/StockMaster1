436fcab07853fc0f3f855042253b4fa091a9d086
j4vierlb <jlarabustos999@gmail.com>
2025-09-07 21:12:32 -0300

    arreglo del router link

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { LoginLogoComponent } from './login-logo/login-logo.component';
 import { LoginHeaderComponent } from './login-header/login-header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -26,7 +27,7 @@ import { ResetPasswordSuccessComponent } from './reset-password-success/reset-pa
   ResetPasswordFormComponent,
   ResetPasswordSuccessComponent
   ],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule, RouterModule],
   exports: [
     LoginLogoComponent,
     LoginHeaderComponent,
