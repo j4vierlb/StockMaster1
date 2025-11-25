eeaf950bf2649f6a654a44c0b6eabbb9f9baec97
j4vierlb <jlarabustos999@gmail.com>
2025-11-24 22:24:18 -0300

    gh-pages: agregar download-app.html

--- iniciar-servidor.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\jlara\Desktop\stockmaster\stockmaster
-echo Iniciando servidor en puerto 8080...
-echo Presiona Ctrl+C para detener
-npx http-server -p 8080 -c 0
-pause
--- src/app/components/login-footer/login-footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-login-footer',
-  templateUrl: './login-footer.component.html',
-  styleUrls: ['./login-footer.component.scss'],
-  standalone: false,
-})
-export class LoginFooterComponent {}
